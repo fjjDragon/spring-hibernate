4578af7ac23ec65527b3a95262256eca32eea93e
Weision <weixx3@139.com>
2018-05-31 07:02:27 +0800

    完善 增删改查

--- src/test/java/com/wxx/test/UserTest.java
+++ src/test/java/com/wxx/test/UserTest.java
@@ -1,6 +1,7 @@
 package com.wxx.test;
 
 import java.util.EnumSet;
+import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -19,14 +20,14 @@ import com.wxx.entity.User;
 public class UserTest {
 	@Test
 	public void createTable() {
-		ServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();  
-	    Metadata metadata = new MetadataSources(registry).buildMetadata();  
-	    SchemaExport export = new SchemaExport();  
-	    export.create(EnumSet.of(TargetType.DATABASE),metadata);  
+		ServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+		Metadata metadata = new MetadataSources(registry).buildMetadata();
+		SchemaExport export = new SchemaExport();
+		export.create(EnumSet.of(TargetType.DATABASE), metadata);
 	}
 
 	@Test
-	public void add() {
+	public void addUser() {
 		// creating configuration object
 		Configuration cfg = new Configuration();
 		cfg.configure("hibernate.cfg.xml");// populates the data of the configuration file
@@ -41,16 +42,87 @@ public class UserTest {
 		Transaction t = session.beginTransaction();
 
 		User user = new User();
-		user.setId(101);
+		user.setId(102);
 		user.setName("Weision");
 		user.setPhone("3322326");
 
-		session.persist(user);// persisting the object
+		// persisting the object
+		// session.persist(user);
+		// session.saveOrUpdate(user);
+		session.save(user);
 
 		t.commit();// transaction is committed
 		session.close();
 
 		System.out.println("successfully saved");
+
+	}
+
+	@Test
+	public void queryUser() {
+		Configuration cfg = new Configuration().configure();
+		SessionFactory factory = cfg.buildSessionFactory();
+		Session session = factory.openSession();
+		Transaction t = session.beginTransaction();
+
+		User user = session.get(User.class, 102);
+
+		t.commit();
+		session.close();
+
+		System.out.println("successfully queryUser: name[" + user.getName() + "]。");
+	}
+
+	@Test
+	public void queryUserList() {
+		Configuration cfg = new Configuration().configure();
+		SessionFactory factory = cfg.buildSessionFactory();
+		Session session = factory.openSession();
+		Transaction t = session.beginTransaction();
+		@SuppressWarnings("unchecked")
+		List<User> users = session.createQuery("from User").getResultList();
+		int i = 0;
+		for (User user : users) {
+			System.out.println("queryUserList[" + i + "] name :[" + user.getName() + "]");
+			i++;
+		}
+		t.commit();
+		session.close();
+	}
+
+	@Test
+	public void deleteUser() {
+		Configuration cfg = new Configuration().configure();
+		SessionFactory factory = cfg.buildSessionFactory();
+		Session session = factory.openSession();
+		Transaction t = session.beginTransaction();
+		User user = new User();
+		user.setId(0);
+		user.setName("Weision");
+		user.setPhone("3322326");
+		
+		session.delete(user);
+		t.commit();
+		session.close();
+		System.out.println("successfully deleted");
+
+	}
+
+	@Test
+	public void updateUser() {
+		Configuration cfg = new Configuration().configure();
+		SessionFactory factory = cfg.buildSessionFactory();
+		Session session = factory.openSession();
+		Transaction t = session.beginTransaction();
+		User user = new User();
+		user.setId(101);
+		user.setName("Json");
+		user.setPhone("3322326");
+		
+		session.update(user);
+		t.commit();
+		session.close();
+		System.out.println("successfully deleted");
 
 	}
 }
